7798b212835236829f7424b00c2cc507a04b729d
lkwsunny <62560458+lkwsunny@users.noreply.github.com>
2021-04-11 04:10:55 +0800

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -2,74 +2,129 @@ import React, { Component } from 'react';
 //import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-import Navbar from "./components/Navbar/navbar";
-import AboutMe from "./components/Aboutme/about-me";
-import ContactMe from "./components/ContactMe/contact-me"
-import "./App.css";
 
-const google = window.google
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-}
+
+import "./App.css";
+import image from "./img/background1.jpg"
+
+
+
 export default class App extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  educationArray = [
+    {
+      label: "The Chinese University of Hong Kong",
+      date: "09/2019-06/2021",
+      button: "fa fa-university fa-2x",
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/8/87/CUHK.svg/300px-CUHK.svg.png",
+      width: "60",
+      height: "50",
+
+    },
+    {
+      label: "Hong Kong Community College",
+      date: "09/2017-06/2019",
+      button: "fa fa-graduation-cap fa-2x",
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b7/PolyU_HKCC.svg/800px-PolyU_HKCC.svg.png",
+      width: "60",
+      height: "50",
+
+    },
+
+    {
+      label: "CCC Ming Kei College",
+      date: "09/2010-06/2017",
+      button: "fa fa-book fa-2x",
+      image: "http://www.cccmkc.edu.hk/sites/all/themes/ntemplate/images/logo.png",
+      width: "230",
+      height: "50",
+    },
+
+
+  ];
+
+  handleSubmit(event) {
+    alert('Thank You, I will contact you later  ');
+    event.preventDefault();
+  }
 
   render() {
     return (
 
-      <div className="about-me-container">
-        <div className= "content-container">
+      <div className="container">
+        <div className="content-container">
           <br></br>
-          <div className="text-container">
-           
-            <p>“One person’s trash is another person’s treasure”.</p>
-            <p>Online Marketplace is an online platform for users to list and view new or used goods for trading. </p>
-            <p>The application allows buyers and sellers to effectively communicate and schedule meets up with easy to understand procedures and features such as the chat box and reminder.</p>
-            <p>Through this platform, we hope to bring convencies for traders and deter online trading scams and fraud.</p>
-      
+          <div className="image-info-container">
+
+            <div className="image-container">
+              <img src={image} alt="personal photo" width="148" height="200" />
             </div>
-          
+
+            <div className="info-container">
+              <p>
+                First Name: Sunny
+                  </p>
+
+              <p>
+                Last Name: Leung
+                  </p>
+
+              <div className="text-container">
+                <p>I am a CUHK year four student who is now studying in computer engineering.
+                During the years in studying computer-related topics, I explored so much interest in programming.
+                I can feel gratification when I completed a different kind of project and this brings me a great dedication to discover more on the theme.
+                Handling several tricky projects had also honed my skills in problem solving and determination, which lays the groundwork for my career in the future.
+                  </p>
+              </div>
+            </div>
+          </div>
           <div className="row">
-            <div className="footnote-container">
-              <h3>HONG KONG OFFICE</h3>
-              <p>123 Open Door Road Suite 1200<br />
-             Orlando, FL 30030<br />
-                <a href="tel:4078981961">407.555.1961</a>
-              </p>
+            <div className="education-container">
+              <h2>Education</h2>
+              <div>
+                {this.educationArray.map((item, index) => {
+                  return (
+                    <div className="array-object" >
+                      <a href={item.link} className="button-item">
+                        <i class={item.button} aria-hidden="true"></i>
+                      </a>
+                      <p>{item.label}</p>
+                      <img src={item.image} alt="personal photo" width={item.width} height={item.height} />
+                    </div>
+                  )
+                })}
+              </div>
             </div>
 
-            <div className="footnote-container">
-              <h3>EMAIL ADDRESS</h3>
-              <a href="mailto:csci3100gp34@gmail.com">csci3100gp34@gmail.com</a>
+            <div className="contact-me-container">
+              <h2>Contact Me</h2>
+              <form onSubmit={this.handleSubmit} >
+                <div className="text-field">
+                  <label >Name</label>
+                  <input type="text" placeholder="Enter your name" />
+                </div>
+
+                <div className="text-field">
+                  <label >Email</label>
+                  <input type="text" placeholder="Enter email address" />
+                </div>
+
+
+                <button type="submit">Login</button>
+              </form>
+
             </div>
 
-            <div className="footnote-container">
-              <h3>SUPPORT</h3>
-              <a href="tel:852999999">852-999-999</a><br />
-              <a href="mailto:csci3100gp34@gmail.com">csci3100gp34@gmail.com</a>
-              <script
-                src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly"
-                async
-              ></script>
-            </div>
           </div>
 
-
-
-          </div>
 
         </div>
-      
+
+      </div>
+
 
 
     )
